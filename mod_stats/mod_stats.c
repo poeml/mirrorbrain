2acaf1ddd82c08677eb2878a347bef8f7aebd8bd
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2007-08-22 06:02:37 +0000

    - in error conditions (reconnection to database after failure), it happens   occasionally that the DBD MySQL driver's pvselect() returns success, but with   result of NULL. If so, ignore the result.

--- mod_stats/mod_stats.c
+++ mod_stats/mod_stats.c
@@ -480,6 +480,11 @@ static int stats_logger(request_rec *r)
             ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, 
                     "[mod_stats] Error looking up %s in database", r->filename);
         }
+        if (res == NULL) {
+            ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, 
+                    "[mod_stats] apr_dbd_pvselect() claimed success, but returned no result");
+            return DECLINED;
+        }
 
         nrows = apr_dbd_num_tuples(dbd->driver, res);
         debugLog(r, cfg, "nrows: %d", nrows);
