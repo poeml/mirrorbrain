3b7e9d1673c6a880e4bf6417f94863e3d11e3dbc
poeml <poeml@users.noreply.github.com>
2008-03-12 18:56:41 +0000

    remove obsolete (because separate) sql index creation statements

--- sql/opensuse-redirector_idx.sql
+++ sql/opensuse-redirector_idx.sql
@@ -1,18 +1,4 @@
--- jw, Fri Jan 19 20:22:42 CET 2007
--- manually added indices
---
-CREATE INDEX file_path_idx ON file (path);
-CREATE INDEX file_server_fileid_idx ON file_server (fileid);
-CREATE INDEX file_server_serverid_idx ON file_server (serverid);
-CREATE INDEX file_server_fileid_serverid_idx ON file_server (fileid,serverid);
 
--- for use_md5:
-ALTER TABLE file_server ADD COLUMN path_md5 BINARY(22);
+-- would this index be useful? TODO: look into which indices we have
 CREATE INDEX file_server_path_md5_idx ON file_server (path_md5);
 
--- for rsync
-ALTER TABLE server ADD COLUMN baseurl_rsync varchar(128);
-ALTER TABLE server ADD COLUMN rsync_is_staged tinyint;
-
--- for peter
-ALTER TABLE server ADD COLUMN comment text;
