61a2e0b190f59efc0fe5ce981d3dc218364079a5
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2008-04-21 16:12:32 +0000

    mod_zrkadlo version 1.7

--- trunk/mod_zrkadlo/mod_zrkadlo.c
+++ trunk/mod_zrkadlo/mod_zrkadlo.c
@@ -66,7 +66,7 @@
 #define UNSET (-1)
 #endif
 
-#define MOD_ZRKADLO_VER "1.6"
+#define MOD_ZRKADLO_VER "1.7"
 #define VERSION_COMPONENT "mod_zrkadlo/"MOD_ZRKADLO_VER
 
 #define DEFAULT_GEOIPFILE "/usr/share/GeoIP/GeoIP.dat"
