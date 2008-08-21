17b684342d29a0763eff755ac9bd7b910a0e9bcd
poeml <poeml@users.noreply.github.com>
2008-08-21 17:05:28 +0000

    make mirrordoctor work with multiple mirrorbrain instances

--- mirrordoctor/mb/conf.py
+++ mirrordoctor/mb/conf.py
@@ -1,9 +1,30 @@
 
 import ConfigParser
+import re
 
-conffile = '/etc/mirrorbrain.conf'
 
-cp = ConfigParser.SafeConfigParser()
-cp.read(conffile)
-config = dict(cp.items('general'))
+class Config:
+
+    def __init__(self, conffile):
+
+        self.config = {}
+
+        cp = ConfigParser.SafeConfigParser()
+        cp.read(conffile)
+
+        self.config = dict(cp.items('general'))
+
+        # transform 'str1, str2, str3' form into a list
+        re_clist = re.compile('[, ]+')
+        self.config['instances'] = [ i.strip() for i in re_clist.split(self.config['instances'].strip()) ]
+
+        instances = self.config['instances']
+        #self.config['default_instance'] = instances[0]
+
+        for i in instances:
+            self.config[i] = dict(cp.items(i))
+        return 
+
+    def get(self):
+        return self.config
 
