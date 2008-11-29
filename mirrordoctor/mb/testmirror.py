1d38cfbaf681a0339394cb4eded08f501cdae42d
poeml <poeml@users.noreply.github.com>
2008-11-29 12:30:24 +0000

    mb tool: add two new commands:  * probefile, stub for a mirror probe.  * export, which exports the servers table of the mirror database for import in    Django, using Django's ORM.

--- mirrordoctor/mb/testmirror.py
+++ mirrordoctor/mb/testmirror.py
@@ -5,3 +5,14 @@ def access_http(url):
     r = urllib2.urlopen(url).read()
     print r
 
+
+def head_req(url):
+
+    req = urllib2.Request(url)
+    req.get_method = lambda: "HEAD"
+
+    try:
+        response = urllib2.urlopen(req)
+        return response.code
+    except:
+        return 0
