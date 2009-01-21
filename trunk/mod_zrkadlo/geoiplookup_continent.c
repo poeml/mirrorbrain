0f1acce3f3c25e2987340dd6d61088e2f471eb2d
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-01-21 00:07:08 +0000

    geoiplookup_continent: make continent lookups work with GeoIPCity databases

--- trunk/mod_zrkadlo/geoiplookup_continent.c
+++ trunk/mod_zrkadlo/geoiplookup_continent.c
@@ -2,26 +2,56 @@
 /* gcc -g -Wall -lGeoIP -o geoiplookup_continent geoiplookup_continent.c */
 #include <stdio.h>
 #include <GeoIP.h>
+#include <GeoIPCity.h>
 
 #define DEFAULT_GEOIPFILE "/usr/share/GeoIP/GeoIP.dat"
 
 int main(int argc, char **argv) {
 	const char *geoipfilename = DEFAULT_GEOIPFILE;
 	GeoIP *gip = NULL;
-	const char *name;
+	GeoIPRecord *gir = NULL;
+	char *name;
+	const char *region_name;
+	int edition, i;
 
-	if (argc != 2) {
-		printf("Usage: geoiplookup_continent <hostname_or_ip>\n");
+	if ((argc != 2) && (argc != 4)) {
+		printf("Usage: geoiplookup_continent [-f custom_file] <hostname_or_ip>\n");
 		return 1;
 	}
-	name = argv[1];
+
+	i = 1;
+	while (i < argc) {
+                if (strcmp(argv[i],"-f") == 0) {
+                        if ((i+1) < argc){
+                                i++;
+                                geoipfilename = argv[i];
+                        }
+                } else {
+			name = argv[i];
+		}
+                i++;
+        }
+
 	gip = GeoIP_open(geoipfilename, GEOIP_STANDARD);
+	edition = GeoIP_database_edition(gip);
 
-	short int country_id = GeoIP_country_id_by_name(gip, name);
-	const char* continent_code = GeoIP_country_continent[country_id];
+	if (edition == GEOIP_COUNTRY_EDITION) {
+		short int country_id = GeoIP_country_id_by_name(gip, name);
+		region_name = GeoIP_country_continent[country_id];
+		printf("%s\n", region_name);
 
-	printf("%s\n", continent_code);
-
+        } else if ((edition == GEOIP_CITY_EDITION_REV0) || 
+		   (edition == GEOIP_CITY_EDITION_REV1)) {
+                gir = GeoIP_record_by_name(gip, name);
+                if (NULL == gir) {
+                        /* printf("%s: IP Address not found\n", GeoIPDBDescription[edition]); */
+                        printf("--\n");
+			return 1;
+                } else {
+                        printf("%s\n", gir->continent_code);
+		}
+	}
+ 
 	return 0;
 }
 
