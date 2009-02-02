2aa009fdb6303343842c66e33bb8c6dadb613167
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-02-02 22:19:45 +0000

    MirrorBrain database scheme for MySQL: - reorder some fields in more logical order - add a few notes about inconsistencies or other room for improvement

--- trunk/sql/mirrorbrain-mysql.sql
+++ trunk/sql/mirrorbrain-mysql.sql
@@ -1,13 +1,9 @@
 
 -- 
--- Database: `redirector`
+-- MirrorBrain Database scheme for MySQL
 -- 
 
 -- --------------------------------------------------------
-
--- 
--- Table structure of table `file`
--- 
 
 CREATE TABLE `file` (
   `id` int(11) unsigned NOT NULL auto_increment,
@@ -18,15 +14,13 @@ CREATE TABLE `file` (
 
 -- --------------------------------------------------------
 
--- 
--- Table structure of table `file_server`
--- 
-
 CREATE TABLE `file_server` (
   `fileid` int(11) unsigned NOT NULL default '0',
   `serverid` int(11) unsigned NOT NULL default '0',
+-- timestamp_file: the default and update trigger are not used
   `timestamp_file` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `timestamp_scanner` timestamp NOT NULL default '0000-00-00 00:00:00',
+-- BUG: This index is a duplicate of file_server_fileid_serverid_idx
   KEY `fileid` (`fileid`,`serverid`),
   KEY `file_server_fileid_idx` (`fileid`),
   KEY `file_server_serverid_idx` (`serverid`),
@@ -35,44 +29,38 @@ CREATE TABLE `file_server` (
 
 -- --------------------------------------------------------
 
--- 
--- Table structure of table `server`
--- 
-
 CREATE TABLE `server` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `identifier` varchar(64) NOT NULL,
   `baseurl` varchar(128) NOT NULL,
   `baseurl_ftp` varchar(128) NOT NULL,
+-- inconsistently, baseurl_rsync can be null - we should get rid of this.
+  `baseurl_rsync` varchar(128) default NULL,
   `enabled` tinyint(1) NOT NULL default '0',
   `status_baseurl` tinyint(1) NOT NULL default '0',
-  `last_scan` timestamp NULL default NULL,
+-- region should be altered to char(2) - as we use it
   `region` varchar(10) default NULL,
   `country` char(2) default NULL,
   `score` int(11) default NULL,
   `scan_fpm` int(11) default NULL,
-  `baseurl_rsync` varchar(128) default NULL,
+  `last_scan` timestamp NULL default NULL,
   `comment` text,
-  `admin_email` text,
-  `netblock` text,
   `admin` text,
+  `admin_email` text,
+  `operator_name` text NOT NULL,
+  `operator_url` text NOT NULL,
+  `netblock` text,
   `lat` float default NULL,
   `lng` float default NULL,
   `country_only` tinyint(1) default '0',
   `region_only` tinyint(1) default '0',
   `other_countries` text NOT NULL,
   `file_maxsize` int(11) NOT NULL default '0',
-  `operator_name` text NOT NULL,
-  `operator_url` text NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `identifier` (`identifier`),
   KEY `server_enabled_status_baseurl_score_idx` (`enabled`,`status_baseurl`,`score`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- 
--- Table structure of table `marker`
--- 
 
 CREATE TABLE `marker` (
   `id` int(11) unsigned NOT NULL auto_increment,
@@ -81,10 +69,6 @@ CREATE TABLE `marker` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- 
--- Table structure of table `countries`
--- 
 
 CREATE TABLE `country` (
   `id` tinyint(1) NOT NULL auto_increment,
