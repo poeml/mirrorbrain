01dc13a68d1fc39b3b2791e5e50e9975a17d5c89
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-02-11 15:16:31 +0000

    mb edit/show:  - add new fields and make them editable mb probefile:  - add --md5 option; when doing probe requests, it'll do actual downloads and    show md5 sums of the responses mb export:  - add --format option  - add postgresql (insert statements) as export format

---
+++ trunk/mirrordoctor/mb/exports.py
@@ -0,0 +1,60 @@
+import time
+
+postgresql_header = """
+--
+-- generated on %s
+--
+""" % time.asctime()
+
+postgresql_template = """\
+--
+-- %(identifier)s
+--
+INSERT INTO server (
+  identifier, baseurl, baseurl_ftp, baseurl_rsync, enabled, status_baseurl, 
+  region, country, asn, prefix,
+  score, comment, operator_name, operator_url, public_notes,
+  admin, admin_email, lat, lng,
+  country_only, region_only, as_only, prefix_only, 
+  other_countries, file_maxsize, scan_fpm)
+VALUES (
+  '%(identifier)s', '%(baseurl)s', '%(baseurlFtp)s', '%(baseurlRsync)s', '%(enabled)s', '%(statusBaseurl)s', 
+  '%(region)s', '%(country)s', '%(asn)s', '%(prefix)s',
+  '%(score)s', $QUOTE$%(comment)s$QUOTE$, $QUOTE$%(operatorName)s$QUOTE$, '%(operatorUrl)s', $QUOTE$%(publicNotes)s$QUOTE$,
+  $QUOTE$%(admin)s$QUOTE$, $QUOTE$%(adminEmail)s$QUOTE$, '%(lat)s', '%(lng)s',
+  '%(countryOnly)s', '%(regionOnly)s', '%(asOnly)s', '%(prefixOnly)s',
+  '%(otherCountries)s', '%(fileMaxsize)s', '%(scanFpm)s');
+"""
+
+django_header = """#!/usr/bin/env python
+import os, sys
+
+mybasepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, mybasepath)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mirrordjango.settings'
+
+from django.db import connection
+
+from mirrordjango.mb.models import Contact, Operator, Project, Server, Mirror
+
+"""
+
+django_template = """\
+
+# ------------------------------------------------------------
+try:
+    c = Contact.objects.get_or_create(username=%(admin)r, password='UNSET', name=%(admin)r, email=%(adminEmail)r)[0]
+except:
+    connection.connection.rollback()
+    c = None
+o = Operator.objects.get_or_create(name='%(identifier)s', logo='')[0]
+p = Project.objects.filter(name='%(project)s')
+p = p[0]
+s = Server.objects.get_or_create(identifier='%(identifier)s', operator=o, region='%(region)s', country='%(country)s', country_only='%(countryOnly)s', region_only='%(regionOnly)s', as_only='%(asOnly)s', prefix_only='%(prefixOnly)s', other_countries=%(otherCountries)r, file_maxsize='%(fileMaxsize)s', comment=%(comment)r, bandwidth=1)[0]
+m = Mirror.objects.get_or_create(http='%(baseurl)s', ftp='%(baseurlFtp)s', rsync='%(baseurlRsync)s', prio='%(score)s', project=p, server=s)[0]
+# s.mirrors.add(m)
+if c:
+    s.contacts.add(c)
+
+"""
+
--- trunk/mirrordoctor/mb/testmirror.py
+++ trunk/mirrordoctor/mb/testmirror.py
@@ -6,6 +6,7 @@ import commands
 import tempfile
 import shutil
 import socket
+import mb.util
 
 TIMEOUT = 20
 
@@ -23,9 +24,11 @@ def dont_use_proxies():
             del os.environ[i]
 
 
-def req(baseurl, filename, http_method='GET'):
+def req(baseurl, filename, http_method='GET', do_digest=False):
 
     url = baseurl + filename
+    worked = False
+    digest = None
 
     if url.startswith('http://') or url.startswith('ftp://'):
         req = urllib2.Request(url)
@@ -35,16 +38,27 @@ def req(baseurl, filename, http_method='GET'):
 
         try:
             response = urllib2.urlopen(req)
-            return response.code
+            worked = True
         except KeyboardInterrupt:
             print >>sys.stderr, 'interrupted!'
             raise
         except:
-            return 0
+            return (0, digest)
+
+        if do_digest:
+            t = tempfile.NamedTemporaryFile()
+            while 1:
+                buf = response.read(1024*512)
+                if not buf: break
+                t.write(buf)
+            t.flush()
+            digest = mb.util.dgst(t.name)
+            t.close()
+
+        return (response.code, digest)
 
     elif url.startswith('rsync://'):
 
-        worked = False
         try:
             tmpdir = tempfile.mkdtemp(prefix='mb_probefile_')
             # note the -r; *some* option is needed because many rsync servers
@@ -52,18 +66,22 @@ def req(baseurl, filename, http_method='GET'):
             # -t (as the most harmless option) also isn't sufficient.
             cmd = 'rsync -r --timeout=%d %s %s/' % (TIMEOUT, url, tmpdir)
             (rc, out) = commands.getstatusoutput(cmd)
-            worked = os.path.exists(os.path.join(tmpdir, os.path.basename(filename)))
+            targetfile = os.path.join(tmpdir, os.path.basename(filename))
+            worked = os.path.exists(targetfile)
+            if worked and do_digest:
+                digest = mb.util.dgst(targetfile)
+
 
         finally:
             shutil.rmtree(tmpdir, ignore_errors=True)
 
         if rc != 0:
-            return 1
+            return (1, digest)
 
         if worked:
-            return 200
+            return (200, digest)
         else:
-            return 0
+            return (0, digest)
 
     else:
         raise 'unknown URL type: %r' % baseurl
