308f173a14a7f86701c087299010719382ed353b
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-03-02 18:00:17 +0000

    mod_mirrorbrain: - reflect an X-Broken-Mirrors header, so these replies can be recognized   easily.

--- trunk/mod_mirrorbrain/mod_mirrorbrain.c
+++ trunk/mod_mirrorbrain/mod_mirrorbrain.c
@@ -1614,6 +1614,7 @@ static int mb_handler(request_rec *r)
         if(apr_table_get(r->headers_in, "X-Broken-Mirrors")) {
             debugLog(r, cfg, "Client sent X-Broken-Mirrors header -- adding broken mirrors");
             apr_table_mergen(r->headers_out, "Cache-Control", "no-store,max-age=0");
+            apr_table_addn(r->headers_out, "X-Broken-Mirrors", "true");
             ap_rprintf(r, "\n      <!-- Broken mirrors for testing: -->\n");
             /* hostname does not resolve */
             ap_rprintf(r, "      <url type=\"http\" location=\"de\" preference=\"%d\">"
