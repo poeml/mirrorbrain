4f58945455eaa0b581f98a3aa2d512da522b811c
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-03-02 18:11:06 +0000

    mod_mirrorbrain: - also set a vary on the X-Broken-Mirrors header. The cache control header   seems to be get lost in the setup on download.opensuse.org

--- trunk/mod_mirrorbrain/mod_mirrorbrain.c
+++ trunk/mod_mirrorbrain/mod_mirrorbrain.c
@@ -1615,6 +1615,7 @@ static int mb_handler(request_rec *r)
             debugLog(r, cfg, "Client sent X-Broken-Mirrors header -- adding broken mirrors");
             apr_table_mergen(r->headers_out, "Cache-Control", "no-store,max-age=0");
             apr_table_addn(r->headers_out, "X-Broken-Mirrors", "true");
+            apr_table_mergen(r->headers_out, "Vary", "X-Broken-Mirrors");
             ap_rprintf(r, "\n      <!-- Broken mirrors for testing: -->\n");
             /* hostname does not resolve */
             ap_rprintf(r, "      <url type=\"http\" location=\"de\" preference=\"%d\">"
