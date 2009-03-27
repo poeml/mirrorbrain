86f8d0ee42f93f1ea200660acf101409bef3c8a7
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-03-27 16:39:16 +0000

    scanner: fix debug message for commits during rsync scans after the commit frequency was adjusted recently

--- trunk/scanner/scanner.pl
+++ trunk/scanner/scanner.pl
@@ -966,7 +966,7 @@ sub rsync_cb
         $name = save_file($name, $priv->{identifier}, $priv->{serverid}, $mtime, $priv->{re});
         $priv->{counter}++;
         if (($priv->{counter} % 50) == 0) {
-          print "$priv->{identifier}: commit after 500 files\n" if $verbose > 1;
+          print "$priv->{identifier}: commit after 50 files\n" if $verbose > 1;
           if($do_transaction) {
             $dbh->commit or die "$DBI::errstr";
           }
