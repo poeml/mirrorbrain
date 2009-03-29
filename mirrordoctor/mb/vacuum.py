514d391c48456726aab92d12af12397fe9a24d29
poeml <poeml@users.noreply.github.com>
2009-03-29 23:43:09 +0000

    mb vacuum: - Make output a little easier to understand. - Remove superfluous semicolons from SQL statements.

--- mirrordoctor/mb/vacuum.py
+++ mirrordoctor/mb/vacuum.py
@@ -4,19 +4,19 @@ def stale(conn):
 
     n_file_total = conn.Filearr.select().count()
 
-    query = "SELECT count(*) FROM filearr WHERE mirrors = '{}';"
+    query = "SELECT count(*) FROM filearr WHERE mirrors = '{}'"
     n_file_stale = conn.Filearr._connection.queryAll(query)[0]
 
 
-    print 'file total:                   %10d' % n_file_total
-    print 'file stale:                   %10d' % n_file_stale
+    print 'Total files:                     %10d' % n_file_total
+    print 'Stale files (not on any mirror): %10d' % n_file_stale
 
 
 def vacuum(conn):
     """delete stale file entries from the database"""
 
     print 'Deleting stale files...'
-    query = "DELETE FROM filearr WHERE mirrors = '{}';"
+    query = "DELETE FROM filearr WHERE mirrors = '{}'"
     conn.Filearr._connection.query(query)
 
     print 'Done.'
