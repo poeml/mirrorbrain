9f5bac8b381a952b5c782fc7e86ff60ff505f9e6
poeml <poeml@users.noreply.github.com>
2009-07-03 12:01:17 +0000

    mb new: - try different geoip database locations (GeoIP database was moved around...).   prefer the larger city lite database, if available, and prefer updated copies   that were fetched with the geoip-lite-update tool.

--- mirrordoctor/mb/geoip.py
+++ mirrordoctor/mb/geoip.py
@@ -1,15 +1,30 @@
+import os
 from subprocess import Popen, PIPE
+
+# try different databases and different locations
+databases = ['/var/lib/GeoIP/GeoLiteCity.dat.updated', 
+             '/var/lib/GeoIP/GeoLiteCity.dat', 
+             '/var/lib/GeoIP/GeoIP.dat.updated',
+             '/var/lib/GeoIP/GeoIP.dat',
+             '/usr/share/GeoIP/GeoLiteCity.dat.updated', 
+             '/usr/share/GeoIP/GeoLiteCity.dat', 
+             '/usr/share/GeoIP/GeoIP.dat.updated',
+             '/usr/share/GeoIP/GeoIP.dat',
+             ]
+for i in databases:
+    if os.path.exists(i):
+        database = i
 
 
 def lookup_country_code(addr):
-    out = Popen(['geoiplookup', addr], stdout=PIPE).communicate()[0]
+    out = Popen(['geoiplookup', '-f', database, addr], stdout=PIPE).communicate()[0]
     out = out.split(':')[1].strip().split(',')[0]
 
     return out.lower()
 
 
 def lookup_region_code(addr):
-    out = Popen(['geoiplookup_continent', addr], stdout=PIPE).communicate()[0]
+    out = Popen(['geoiplookup_continent', '-f', database, addr], stdout=PIPE).communicate()[0]
 
     return out.strip().lower()
 
