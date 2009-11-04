673f6a4358d5333cb005e90fcda810685a4e3086
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-11-04 12:44:03 +0000

    bump version (2.10.2)

--- trunk/mod_mirrorbrain/mod_mirrorbrain.c
+++ trunk/mod_mirrorbrain/mod_mirrorbrain.c
@@ -73,7 +73,7 @@
 #define UNSET (-1)
 #endif
 
-#define MOD_MIRRORBRAIN_VER "2.10.0"
+#define MOD_MIRRORBRAIN_VER "2.10.2"
 #define VERSION_COMPONENT "mod_mirrorbrain/"MOD_MIRRORBRAIN_VER
 
 #ifdef NO_MOD_GEOIP
