d81e2f22f082da5a2c43bbea69a17005d2a4c686
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-11-27 23:18:12 +0000

    bump version (2.10.3)

--- trunk/mirrordoctor/setup.py
+++ trunk/mirrordoctor/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='mirrordoctor',
-      version='1.0',
+      version='1.1',
       description='MirrorDoctor, a tool to maintain the MirrorBrain database',
       author='MirrorBrain project',
       author_email='info@mirrorbrain.org',
