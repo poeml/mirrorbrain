poeml/mirrorbrain

commit f39cc41d314eb08f9a4c9fefa7f781fb4480de56

mod_mirrorbrain.c: - detect the APR version at compile time, and if it's 1.2, change the way how   DBD database rows are counted. Thereby fixing http://mirrorbrain.org/issues/issue7
git-svn-id: http://svn.mirrorbrain.org/svn/mirrorbrain/trunk@7887 2d291e14-ff0c-0410-a3fa-d5caa9871f55

mod_mirrorbrain/mod_mirrorbrain.c
@@ -1170,7 +1170,19 @@ static int mb_handler(request_rec *r)
         const char *val = NULL;
         short col = 0; /* incremented for the column we are reading out */
 
-        rv = apr_dbd_get_row(dbd->driver, r->pool, res, &row, i);
+        rv = apr_dbd_get_row(dbd->driver, r->pool, res, &row, 
+#if (APR_MAJOR_VERSION == 1 && APR_MINOR_VERSION == 2)
+                             /* APR 1.2 was the first version to support the DBD
+                              * framework, and had a different way of counting
+                              * rows, see http://mirrorbrain.org/issues/issue7
+                              * */
+                             i - 1
+#else
+                             i
+#endif
+                             );
+
+
         if (rv != 0) {
             ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r,
                       "[mod_mirrorbrain] Error looking up %s in database", filename);

commit 559de3c06372ca89d97c13807563ce57d7631799

mod_mirrorbrain: - add missing include on apr_version.h, because otherwise the compile time   detection for the APR version is not effective. This fixes a regression   introduced with r7887. See http://mirrorbrain.org/issues/issue29

mod_mirrorbrain/mod_mirrorbrain.c
@@ -41,6 +41,7 @@
 #include "http_protocol.h"
 #include "util_md5.h"
 
+#include "apr_version.h"
 #include "apr_strings.h"
 #include "apr_lib.h"
 #include "apr_fnmatch.h"