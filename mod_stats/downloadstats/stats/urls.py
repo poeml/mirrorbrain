5aa02e77983fb4c5dda17888d17747e185065bdd
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2009-12-17 15:37:40 +0000

    add a view that doesn't split by country

--- mod_stats/downloadstats/stats/urls.py
+++ mod_stats/downloadstats/stats/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('downloadstats.stats.views',
 
     (r'^csv/(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})\.csv$', 'stats_csv'),
-    (r'^csv/(?P<year>\d{4})(?P<month>\d{2})\.csv$', 'stats_csv'),
+    (r'^csv/(?P<year>\d{4})(?P<month>\d{2})\.csv$',               'stats_csv'),
+
+    (r'^csv/all-countries/(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})\.csv$', 'stats_csv', {'by_country': False}),
+    (r'^csv/all-countries/(?P<year>\d{4})(?P<month>\d{2})\.csv$',               'stats_csv', {'by_country': False}),
 )
 
--- mod_stats/downloadstats/stats/views.py
+++ mod_stats/downloadstats/stats/views.py
@@ -1,38 +1,45 @@
-# Create your views here.
-
 from django.http import HttpResponse
 from django.db.models import Sum
-#from django.views.decorators.cache import cache_page
 
 from downloadstats.stats.models import Counter
 
 
-#@cache_page(60*15)
-def stats_csv(request, year, month, day=None):
+def stats_csv(request, year, month, day=None, by_country=True):
+    import csv
 
     response = HttpResponse(mimetype='text/plain')
     #response['Content-Disposition'] = 'attachment; filename=%s%s%s.csv' % (year, month, day or '')
 
-    import csv
     writer = csv.writer(response)
-    writer.writerow(['Date', 'Product', 'Version', 'OS', 'Language', 'Country', 'Downloads'])
+    if by_country:
+        writer.writerow(['Date', 'Product', 'Version', 'OS', 'Language', 'Country', 'Downloads'])
+    else:
+        writer.writerow(['Date', 'Product', 'Version', 'OS', 'Language', 'Downloads'])
 
 
-    products = Counter.objects.values('product').distinct()
+    products = [ i['product'] for i in Counter.objects.values('product').distinct() ]
+
     for product in products:
 
-        s = Counter.objects.filter(product=product['product'])
+        s = Counter.objects.filter(product=product)
         s = s.filter(date__year=year, date__month=month)
         if day:
             s = s.filter(date__day=day)
-        s = s.values('date', 'product', 'version', 'osname', 'lang', 'country')
+        if by_country:
+            s = s.values('date', 'product', 'version', 'osname', 'lang', 'country')
+        else:
+            s = s.values('date', 'product', 'version', 'osname', 'lang')
         s = s.annotate(counter=Sum('count'))
         s = s.order_by('date', 'product')
 
         for i in s:
-            writer.writerow((i['date'], i['product'], i['version'], i['osname'], i['lang'], i['country'], i['counter']))
+            if by_country:
+                writer.writerow((i['date'], i['product'], i['version'], i['osname'], i['lang'], i['country'], i['counter']))
+            else:
+                writer.writerow((i['date'], i['product'], i['version'], i['osname'], i['lang'], i['counter']))
 
     return response
+
 
 
 
