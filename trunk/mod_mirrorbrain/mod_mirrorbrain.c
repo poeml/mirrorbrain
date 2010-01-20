59082f308722decbd75d62ac23a55a4822ccf5fa
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2010-01-20 18:07:44 +0000

    docs/installation: - the example for creating metalink hashes was wrong. Fix it and add some more details.

--- trunk/mod_mirrorbrain/mod_mirrorbrain.c
+++ trunk/mod_mirrorbrain/mod_mirrorbrain.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007,2008,2009 Peter Poeml / Novell Inc.
+ * Copyright 2007,2008,2009,2010 Peter Poeml / Novell Inc.
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
