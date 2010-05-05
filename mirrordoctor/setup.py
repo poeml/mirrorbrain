e4191eadac9f66c200a3d6b0da46c947c6510835
poeml <poeml@users.noreply.github.com>
2010-05-05 19:43:41 +0000

    renaming the somewhat epic mirrordoctor.py to mb.py, in order to reduce possible confusion.

--- mirrordoctor/setup.py
+++ mirrordoctor/setup.py
@@ -2,16 +2,16 @@
 
 from distutils.core import setup, Extension
 
-setup(name='mirrordoctor',
+setup(name='mb',
       version='2.13.0',
-      description='MirrorDoctor, a tool to maintain the MirrorBrain database',
+      description='mb, a tool to maintain the MirrorBrain database',
       author='MirrorBrain project',
       author_email='info@mirrorbrain.org',
       license='GPLv2',
       url='http://mirrorbrain.org/',
 
       packages=['mb'],
-      scripts=['mirrordoctor.py'],
+      scripts=['mb.py'],
 
       ext_modules=[Extension('zsync', sources=['zsyncmodule.c'])],
      )
