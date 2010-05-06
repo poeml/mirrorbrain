3890ad960e82eb56a22265b613a57d4e3b793601
poeml <poeml@users.noreply.github.com>
2010-05-06 16:18:16 +0000

    mb module: Since the "mb" script has the same name as the Python module, it'd try to import itself as long as there's mb.py in the script's directory. Therefore, we remove mb.py and only leave mb. Don't know if there's a better way to achieve this...

--- mb/setup.py
+++ mb/setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup, Extension
 
-setup(name='mb',
+s = setup(name='mb',
       version='2.13.0',
       description='mb, a tool to maintain the MirrorBrain database',
       author='MirrorBrain project',
@@ -15,3 +15,15 @@ setup(name='mb',
 
       ext_modules=[Extension('zsync', sources=['zsyncmodule.c'])],
      )
+
+
+# Since the "mb" script has the same name as the Python module, it'd
+# try to import itself as long as there's mb.py in the script's directory.
+# Therefore, we remove mb.py and only leave mb.
+# Don't know if there's a better way to achieve this...
+from distutils.command.install_scripts import install_scripts
+script_install_dir = s.get_command_obj('install_scripts').install_dir
+
+from os.path import join
+from os import unlink
+unlink(join(script_install_dir, 'mb.py'))
