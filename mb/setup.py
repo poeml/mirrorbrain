7cee622d91121ff18b7dff54c59bf519d3707b26
poeml <poeml@users.noreply.github.com>
2010-09-19 18:18:55 +0000

    bump version (2.13.2)

--- mb/setup.py
+++ mb/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup, Extension
 
 s = setup(name='mb',
-      version='2.13.1',
+      version='2.13.2',
       description='mb, a tool to maintain the MirrorBrain database',
       author='MirrorBrain project',
       author_email='info@mirrorbrain.org',
