c973de69433aafb4436c5323d1c7546d0e1d535c
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2010-09-25 03:00:29 +0000

    mb makehashes: - same as in mod_mirrorbrain, the md5 key in torrents needs to be in hex, not   binary. Issue #77.

--- trunk/mb/mb/hashes.py
+++ trunk/mb/mb/hashes.py
@@ -453,7 +453,7 @@ class HashBag:
                  '4:name', str(len(self.basename)), ':', self.basename, 
                  '12:piece length', 'i', str(self.chunk_size), 'e',
                  '6:pieces', str(len(self.pieces) * SHA1_DIGESTSIZE), ':', ''.join(self.pieces),
-                 '3:md5', str(MD5_DIGESTSIZE), ':', self.md5,
+                 '6:md5sum', str(MD5_DIGESTSIZE * 2), ':', self.md5hex,
                  '4:sha1', str(SHA1_DIGESTSIZE), ':', self.sha1,
                  '6:sha256', str(SHA256_DIGESTSIZE), ':', self.sha256,
                'e']
