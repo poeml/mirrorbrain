9e8b50f6201117b1b01d675a61734f1b0260bb74
poeml <poeml@users.noreply.github.com>
2012-04-11 21:20:45 +0000

    mb makehashes: - fixing issue #94 by escaping string literals in the regular expression that we use to   search for existing files in the hash table. Otherwise they would be used as   regexp themselves by PostgreSQL. Thanks KDE sysadmins for help!

--- mb/mb/files.py
+++ mb/mb/files.py
@@ -1,5 +1,6 @@
 from sqlobject.sqlbuilder import AND
 
+from mb import util
 
 def has_file(conn, path, mirror_id):
     """check if file 'path' exists on mirror 'mirror_id'
@@ -156,7 +157,7 @@ def dir_filelist(conn, path):
                    FROM filearr 
                LEFT JOIN hash 
                    ON hash.file_id = filearr.id 
-               WHERE filearr.path ~ '^%s/[^/]*$'""" % path
+               WHERE filearr.path ~ '^""" + util.pgsql_regexp_esc(path) +"""/[^/]*$'""" 
 
     result = conn.Server._connection.queryAll(query)
     return result
