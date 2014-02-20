d3836f8bbe30e6460930f57f38b631ab839e1577
poeml <poeml@2d291e14-ff0c-0410-a3fa-d5caa9871f55>
2014-02-20 01:15:32 +0000

    mb/mb: add a function to quickly find any file in a local file tree (just picking the first one encountered) for further tests.

---
+++ trunk/mb/mb/randomfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import os
+import os.path
+
+# python equivalent for this:
+# find /srv/mirrors/openoffice -type f | head -n 1
+
+# needs local file tree and knowledge of base dir
+
+
+def find_first_file_in_tree(bdir):
+    files = None
+    for (path, dirs, files) in os.walk(bdir):
+        #print path
+        #print dirs 
+        #print files 
+        #print "----"
+        if files:
+            break
+    #print '********'
+    if files:
+        found = os.path.join(path, files[0])
+        found = found[len(bdir):]
+        found = found.lstrip('/')
+        return found
+    else:
+        return None
+
+
+if __name__ == '__main__':
+
+    bdir = sys.argv[1]
+    print find_first_file_in_tree(bdir)
+
